c935612d50d902e8fea892b10ed1fee75a2a029b
Akshat8776 <93507527+Akshat8776@users.noreply.github.com>
2024-02-09 17:14:47 +0530

    first comm

--- lib/cubits/loading_cubit.dart
+++ lib/cubits/loading_cubit.dart
@@ -1,20 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '/enums/states_enum.dart';
 part 'loading_state.dart';
-class LoadingCubit extends Cubit<LoadingState>{
-  LoadingCubit() :super(LoadingState(current_state: Current_State.NO_ITEM));
+
+class LoadingCubit extends Cubit<LoadingState> {
+  LoadingCubit() : super(LoadingState(current_state: Current_State.IDLE));
 
   void perform() {
-    print('perform is called');
+    // print('perform is called');
     emit(LoadingState(current_state: Current_State.SEARCHING));
   }
+
   void noInternet() {
-    print(2);
+    // print(2);
     emit(LoadingState(current_state: Current_State.NO_INTERNET));
   }
+
   void free() {
     emit(LoadingState(current_state: Current_State.IDLE));
   }
+
   void noItem() {
     emit(LoadingState(current_state: Current_State.NO_ITEM));
   }
